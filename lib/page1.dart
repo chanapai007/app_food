119d8eef5d8c53e2e578730b26954372687e8584
chanapai007 <chimpanzeeman53@gmail.com>
2025-01-22 10:39:47 +0700

    กหฟก

--- lib/page1.dart
+++ lib/page1.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-
 class FoodDetailPage extends StatefulWidget {
   final String title;
   final String imageUrl;
--- lib/page2.dart
+++ lib/page2.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-
-
 class SnackDetailPage extends StatefulWidget {
   final String title;
   final String imageUrl;
