dcbd524a580b9af2285956bc709b74532b466ad1
chanapai007 <chimpanzeeman53@gmail.com>
2025-02-04 11:28:40 +0700

    fhgs

--- lib/page1.dart
+++ lib/page1.dart
@@ -16,7 +16,7 @@ class FoodDetailPage extends StatefulWidget {
 
   @override
   // ignore: library_private_types_in_public_api
-  _FoodDetailPageState createState() => _FoodDetailPageState();
+  _FoodDetailP ageState createState() => _FoodDetailPageState();
 }
 
 class _FoodDetailPageState extends State<FoodDetailPage> {
